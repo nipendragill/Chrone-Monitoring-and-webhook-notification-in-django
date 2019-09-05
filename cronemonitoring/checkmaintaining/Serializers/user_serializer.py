7199cdbefb711e2d9fff231cc9f10e90855b4e1f
nipendragill <nipendra9457@gmail.com>
2019-09-05 21:23:12 +0530

    added code for user update and getting info for particular user

---
+++ cronemonitoring/checkmaintaining/Serializers/user_serializer.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from ..models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta(object):
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'password', 'created_at', 'updated_at')
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+class UserProfileWriteSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'first_name', 'last_name', 'password', 'created_at', 'updated_at')
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileWriteSerializer, self).__init__(*args, **kwargs)
+        fields_ = ['email', 'first_name', 'last_name', 'password']
+
+        for field in fields_:
+            self.fields[field].error_messages = {
+                'required': '{0} is required.'.format(field.upper()),
+                'blank': '{0} cannot be blank.'.format(field.upper()),
+                'invalid_choice': 'Invalid entry for {0}'.format(field.upper())
+            }
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get('email', instance.email)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+
+        instance.save()
+        return instance
--- cronemonitoring/checkmaintaining/views.py
+++ cronemonitoring/checkmaintaining/views.py
@@ -1,3 +1,187 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .Serializers.user_serializer import UserSerializer, UserProfileWriteSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from .models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework_jwt.settings import api_settings
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from .pagination import CustomPagination
+from django.db import transaction, DatabaseError
+# from rest_framework import request
+from .error import Error
+# from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+# from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny, ])
+def authenticate_user(request):
+    try:
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.get(email=email, password=password)
+        if user:
+            try:
+                jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+                jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+                payload = jwt_payload_handler(user)
+                token = jwt_encode_handler(payload)
+
+                user_details = {}
+                user_details['name'] = f'{user.first_name} {user.last_name}'
+                user_details['token'] = token
+                user_details['email'] = user.email
+                user_details['created_at'] = user.created_at
+                user_details['updated_at'] = user.updated_at
+                return Response(user_details, status=status.HTTP_200_OK)
+
+            except Exception as e:
+                raise e
+        else:
+            res = {
+                'error': 'account is currently inactive, please activate your account'}
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+    except KeyError:
+        res = {'error': 'pleasae provide email address and password'}
+        return Response(res)
+
+
+class CreateUserAPIView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        user = request.data
+        transaction.set_autocommit(False)
+        try:
+            serializer = UserSerializer(data=user)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                transaction.commit()
+                transaction.set_autocommit(True)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                transaction.rollback()
+                transaction.set_autocommit(False)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except DatabaseError as e:
+            transaction.rollback()
+            transaction.set_autocommit(True)
+            return Response({'detail': 'Error inserting database'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class ListUserAPIView(generics.ListAPIView):
+
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        try:
+            queryset = User.objects.all()
+            queryset = self.paginate_queryset(queryset)
+            return queryset, None
+        except DatabaseError as e:
+            return Response({'detail': 'Error getting User'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def get(self, request):
+        data, error = self.get_queryset()
+
+        if error is not None:
+            return Response({'detail': error.message},
+                            status=status.HTTP_400_BAD_REQUEST)
+        serialized_data = UserSerializer(data, many=True).data
+        return self.get_paginated_response(serialized_data)
+
+
+class UpdateUserAPIView(generics.RetrieveUpdateAPIView):
+
+    permission_classes = (IsAuthenticated, )
+
+    def get_queryset(self, user_id = None):
+
+        try:
+            user_info = User.objects.filter(pk=user_id)
+            if user_info.exists():
+               return user_info, None
+            else:
+                error = Error({'detail':'UserId does not exists'},
+                              status=status.HTTP_400_BAD_REQUEST)
+                return None, error
+
+        except DatabaseError as e:
+            error = Error({"detail":'Error fetching details of the user'},
+                          status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return None, error
+
+    def get(self, request, *args, **kwargs):
+
+        user_id = kwargs.get('user_id')
+        if user_id is None:
+            return Response({'detail':'Please select a user id'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        user_id = int(user_id)
+        user, error = self.get_queryset(user_id=user_id)
+        if error is not None:
+            return Response({'detail': error.message},
+                            status=error.status)
+
+        serialzer_class = UserSerializer(user.first())
+        return Response(serialzer_class.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        user_id = kwargs.get('user_id')
+        if user_id is None:
+            return Response({'detail': 'Please select a user id'},
+                            status=status.HTTP_400_BAD_REQUEST)
+        user_id = int(user_id)
+        user, error = self.get_queryset(user_id=user_id)
+        if error is not None:
+            return Response({'detial': error.message},
+                            status=status.HTTP_400_BAD_REQUEST)
+        user = user.first()
+
+        transaction.set_autocommit(False)
+        try:
+            context = {"request": self.request,
+                       "updated_by": request.user}
+            serializer = UserProfileWriteSerializer(user, data=request.data, partial=True,
+                                                    context=context)
+            if serializer.is_valid(raise_exception=True):
+                serializer.update(user, request.data)
+                transaction.commit()
+                transaction.set_autocommit(True)
+                serializer = UserSerializer(user)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except DatabaseError as e:
+            return Response({'detail': 'Error connecting to database'},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+        # try:
+        #     user = User.objects.get(pk=user_id)
+        #     data = request.data
+        #     print(data, ' is the data')
+        #     for key in data.keys():
+        #         setattr(user, key, data[key])
+        #         user.save()
+        #     serializer = UserSerializer(data=user)
+        #     if serializer.is_valid(raise_exception=True):
+        #         transaction.commit()
+        #         transaction.set_autocommit(True)
+        #         return Response(serializer.data, status=status.HTTP_200_OK)
+        #     else:
+        #         transaction.rollback()
+        #         transaction.set_autocommit(False)
+        #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # except DatabaseError as ex:
+        #     transaction.rollback()
+        #     transaction.set_autocommit(False)
+        #     return Response({'Detail':'DataBase error occured'},
+        #                     status=status.HTTP_500_INTERNAL_SERVER_ERROR)
